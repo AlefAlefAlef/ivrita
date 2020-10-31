4b19bed9e647fc9921e463a39154411a2aba20ff
Reuven Karasik <rubik@karasik.org>
2020-10-31 08:55:04 +0200

    Moved rules tests to a separate file

---
+++ tests/rules.test.js
@@ -0,0 +1,36 @@
+import Ivrita from '../src/ivrita';
+
+test('Word beginnings', () => {
+  expect(Ivrita.genderize('א.נשים', Ivrita.FEMALE)).toBe('נשים');
+  expect(Ivrita.genderize('אנשי/ות עסקים', Ivrita.FEMALE)).toBe('נשות עסקים');
+});
+
+test('Skip niqqud words', () => {
+  expect(Ivrita.genderize('מעצּבֻּ/תֻ', Ivrita.FEMALE)).toBe('מעצּבֻּ/תֻ');
+});
+
+test('Whole words', () => {
+  // Female
+  expect(Ivrita.genderize('רוצ/י', Ivrita.FEMALE)).toBe('רוצי');
+  expect(Ivrita.genderize('קומ/י', Ivrita.FEMALE)).toBe('קומי');
+  expect(Ivrita.genderize('עופ/י', Ivrita.FEMALE)).toBe('עופי');
+  expect(Ivrita.genderize('שים/י', Ivrita.FEMALE)).toBe('שימי');
+  expect(Ivrita.genderize('אח/ות', Ivrita.FEMALE)).toBe('אחות');
+  expect(Ivrita.genderize('לו/לה, לה/לו', Ivrita.FEMALE)).toBe('לה, לה');
+  expect(Ivrita.genderize('הוא/היא, היא/הוא', Ivrita.FEMALE)).toBe('היא, היא');
+  expect(Ivrita.genderize('אנשי/ות', Ivrita.FEMALE)).toBe('נשות');
+  expect(Ivrita.genderize('מישהו/י', Ivrita.FEMALE)).toBe('מישהי');
+  expect(Ivrita.genderize('אחד/ת, אחד/אחת', Ivrita.FEMALE)).toBe('אחת, אחת');
+
+  // Male
+  expect(Ivrita.genderize('רוצ/י', Ivrita.MALE)).toBe('רוץ');
+  expect(Ivrita.genderize('קומ/י', Ivrita.MALE)).toBe('קום');
+  expect(Ivrita.genderize('עופ/י', Ivrita.MALE)).toBe('עוף');
+  expect(Ivrita.genderize('שים/י', Ivrita.MALE)).toBe('שים');
+  expect(Ivrita.genderize('אח/ות', Ivrita.MALE)).toBe('אח');
+  expect(Ivrita.genderize('לו/לה, לה/לו', Ivrita.MALE)).toBe('לו, לו');
+  expect(Ivrita.genderize('הוא/היא, היא/הוא', Ivrita.MALE)).toBe('הוא, הוא');
+  expect(Ivrita.genderize('אנשי/ות', Ivrita.MALE)).toBe('אנשי');
+  expect(Ivrita.genderize('מישהו/י', Ivrita.MALE)).toBe('מישהו');
+  expect(Ivrita.genderize('אחד/ת, אחד/אחת', Ivrita.MALE)).toBe('אחד, אחד');
+});
