366eb3df2080f8bcf302061fbe2feb16c3a49d97
Reuven Karasik <rubik@karasik.org>
2020-11-17 18:29:26 +0200

    Added ןם to Ivrita syntax const

--- src/utils/characters.js
+++ src/utils/characters.js
@@ -9,4 +9,4 @@ export const W = `[א-ת${G}]`;
 export const FIN = `[${finals}]`;
 export const B = `(?=^|$|\b|[^א-ת${G}?\u0590-\u05BD\u05BF\u05C1-\u05C2\u05C4-\u05C7\u05EF-\u05F2\uFB2A-\uFB4F])`; // Boundary, like "\b" in regex. All unicode characters which can be part of a hebrew word
 
-export const SYNTAX = [EXTSEP, '\\[', '\\{', '\\('].join('|');
+export const SYNTAX = [EXTSEP, '\\[', '\\{', '\\(', 'םן', 'ןם', 'יםות', 'ותים'].join('|');
