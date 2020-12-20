c6b3684e2d805e8d5720a13421d69874a5ac8752
Reuven Karasik <rubik@karasik.org>
2020-12-20 16:59:48 +0200

    Split tests for attributes and added a test for custom attributes

--- test/attributes.test.js
+++ test/attributes.test.js
@@ -9,7 +9,7 @@ const template = `
       <input type="text" id="name" placeholder="הכנס/י את שמך כאן" />
       <input type="text" id="city" placeholder="הכנס/י את העיר שבה את/ה גר/ה כאן" />
       <a href="#" title="לחץ/י כאן כדי להתנתק" id="logout">התנתק/י</a>
-      <button>נקה/י את הטופס</button>
+      <button data-custom-text="לא יודע/ת איך?">נקה/י את הטופס</button>
       <input type="submit" value="שלח/י" />
       <input type="button" value="בטל/י" />
     </form>
@@ -20,7 +20,27 @@ test('Input placeholder', () => {
   document.body.innerHTML = template;
   const name = document.getElementById('name');
   const city = document.getElementById('city');
+
+  const ivrita = new Ivrita(document.body);
+  ivrita.setMode(FEMALE);
+
+  expect(name.getAttribute('placeholder')).toBe('הכניסי את שמך כאן');
+  expect(city.getAttribute('placeholder')).toBe('הכניסי את העיר שבה את גרה כאן');
+});
+
+test('Link title', () => {
+  document.body.innerHTML = template;
   const link = document.querySelector('a');
+
+  const ivrita = new Ivrita(document.body);
+  ivrita.setMode(FEMALE);
+
+  expect(link.innerHTML).toBe('התנתקי');
+  expect(link.getAttribute('title')).toBe('לחצי כאן כדי להתנתק');
+});
+
+test('Buttons', () => {
+  document.body.innerHTML = template;
   const button = document.querySelector('button');
   const inputSubmit = document.querySelector('input[type=submit]');
   const inputButton = document.querySelector('input[type=button]');
@@ -28,11 +48,19 @@ test('Input placeholder', () => {
   const ivrita = new Ivrita(document.body);
   ivrita.setMode(FEMALE);
 
-  expect(name.getAttribute('placeholder')).toBe('הכניסי את שמך כאן');
-  expect(city.getAttribute('placeholder')).toBe('הכניסי את העיר שבה את גרה כאן');
-  expect(link.innerHTML).toBe('התנתקי');
-  expect(link.getAttribute('title')).toBe('לחצי כאן כדי להתנתק');
   expect(button.innerHTML).toBe('נקי את הטופס');
   expect(inputSubmit.value).toBe('שלחי');
   expect(inputButton.value).toBe('בטלי');
+});
+
+test('Custom attribute', () => {
+  document.body.innerHTML = template;
+  const button = document.querySelector('button');
+
+  const ivrita = new Ivrita(document.body);
+  ivrita.relavantAttributes['button[data-custom-text]'] = ['data-custom-text'];
+  ivrita.registerTextAttributes(document.body);
+  ivrita.setMode(FEMALE);
+
+  expect(button.getAttribute('data-custom-text')).toBe('לא יודעת איך?');
 });
