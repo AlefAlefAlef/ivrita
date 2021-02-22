bc5d49307036d2dc34d47bfd31637524c65645ae
Reuven Karasik <rubik@karasik.org>
2021-02-22 23:30:30 +0200

    CustomSwitch functionality - derives from Switch master class

---
+++ src/ui/custom.js
@@ -0,0 +1,85 @@
+// eslint-disable-next-line no-unused-vars
+import dom from 'jsx-render';
+import IvritaElement from '../element';
+import { MALE, FEMALE, NEUTRAL } from '../ivrita';
+import IvritaSwitch from './switch';
+
+export default class CustomSwitch extends IvritaSwitch {
+  buttons = new Map();
+
+  constructor(element, buttonsSelector, ...ivritaInstances) {
+    if (ivritaInstances.length === 0 && typeof window._ivrita !== 'undefined') {
+      super(window._ivrita);
+    } else {
+      super(...ivritaInstances);
+    }
+
+    this.element = element;
+    this.buttonsSelector = buttonsSelector;
+
+    this.listenForClicks();
+    this.listenForExternalChanges();
+  }
+
+  listenForClicks() {
+    Array.from(this.element.querySelectorAll(this.buttonsSelector)).forEach((button) => {
+      const btnModeStr = button.dataset.ivritaMode;
+      let btnMode;
+      switch (btnModeStr.toUpperCase()) {
+        case 'MALE':
+          btnMode = MALE;
+          break;
+
+        case 'FEMALE':
+          btnMode = FEMALE;
+          break;
+
+        case 'NEUTRAL':
+          btnMode = NEUTRAL;
+          break;
+
+        default:
+          btnMode = null;
+      }
+
+      if (btnMode !== null) {
+        button.addEventListener('click', () => {
+          this.setMode(btnMode);
+          this.setActiveButton(btnMode);
+        });
+
+        this.buttons.set(button, btnMode);
+      }
+    });
+  }
+
+  listenForExternalChanges() {
+    this.ivritaInstances.forEach((ivritaInstance) => {
+      if (ivritaInstance.elements.length) {
+        ivritaInstance.elements[0].addEventListener(IvritaElement.EVENT_MODE_CHANGED,
+          ({ detail: { mode } }) => {
+            this.setActiveButton(mode);
+          });
+      }
+    });
+  }
+
+  setActiveButton(newMode) {
+    this.buttons.forEach((value, btn) => {
+      if (newMode === value) {
+        btn.classList.add('ivrita-active');
+      } else {
+        btn.classList.remove('ivrita-active');
+      }
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  build() {}
+
+  // eslint-disable-next-line class-methods-use-this
+  rebuild() {}
+
+  // eslint-disable-next-line class-methods-use-this
+  render() {}
+}
--- src/ui/switch.js
+++ src/ui/switch.js
@@ -1,42 +1,10 @@
 import JSXComponent from 'jsx-render/lib/JSXComponent';
-// eslint-disable-next-line no-unused-vars
-import dom from 'jsx-render';
-import {
-  iconTitle,
-  aboutLinkText,
-  defaultMaleLabel,
-  defaultFemaleLabel,
-  defaultNeutralLabel,
-} from './hebrew';
-
-import './style.scss';
-
-import Ivrita from '../element';
 
 export default class IvritaSwitch extends JSXComponent {
-  config = {
-    position: 'left',
-    iconTitle,
-    aboutLinkText,
-    aboutLinkURL: 'https://alefalefalef.co.il/ivrita/',
-    style: 0,
-    logoIcon: '&#x26A5;&#xFE0E;',
-    modes: {
-      [Ivrita.MALE]: {
-        label: defaultMaleLabel,
-        icon: '&#x2642;&#xFE0E;',
-      },
-      [Ivrita.FEMALE]: {
-        label: defaultFemaleLabel,
-        icon: '&#x2640;&#xFE0E;',
-      },
-      [Ivrita.NEUTRAL]: {
-        label: defaultNeutralLabel,
-        icon: '&#x26A5;&#xFE0E;',
-      },
-    },
-    default: Ivrita.NEUTRAL,
-  }
+  /**
+   * The name of the event to be dispatched on `document` before reading the configuration
+   */
+  static EVENT_INIT = 'ivrita-ui-ready';
 
   /**
    * @property {Ivrita[]} ivritaInstances
@@ -60,40 +28,13 @@ export default class IvritaSwitch extends JSXComponent {
     this.ivritaInstances = ivritaInstances;
   }
 
-  setMode(modeStr) {
-    const mode = parseInt(modeStr, 10);
-    this.element.querySelectorAll('a.ivrita-mode-changer').forEach((e) => {
-      if (parseInt(e.dataset.ivritaMode, 10) === mode) {
-        e.classList.add('ivrita-active');
-      } else {
-        e.classList.remove('ivrita-active');
-      }
-    });
-    this.element.querySelector(`a[data-ivrita-mode="${mode}"]`).classList.add('ivrita-active');
+  /**
+   * Sets the mode for all ivritaInstances of this switch
+   * @param {string} modeStr The new mode
+   */
+  setMode(mode) {
     this.ivritaInstances.forEach((i) => i.setMode(mode));
-
     window.localStorage.setItem('ivrita-mode', mode);
-  }
-
-  render() {
-    return (
-      <div class={`ivrita-switch ivrita-switch--${this.config.position}`}>
-        <a href="#" class="ivrita-logo" title={ this.config.iconTitle } dangerouslySetInnerHTML={{ __html: this.config.logoIcon }}></a>
-        {
-          Object.keys(this.config.modes).map((mode) => (
-            <a href="#"
-              class={`ivrita-mode-changer ivrita-button ivrita-button-style-${this.config.style}`}
-              data-ivrita-mode={ mode }
-              title={ this.config.modes[mode].label }
-              ref={ super.ref }
-              onClick={ () => { this.setMode(mode); } }
-              dangerouslySetInnerHTML={{ __html: this.config.modes[mode].icon }}
-              ></a>
-          ))
-        }
-        <a href={ this.config.aboutLinkURL } class="ivrita-info-link" title={ this.config.aboutLinkText } target="_blank">ⓘ</a>
-      </div>
-    );
   }
 
   build() {
@@ -101,6 +42,9 @@ export default class IvritaSwitch extends JSXComponent {
   }
 
   init() {
+    // Dispatch the event, in order to allow external reconfiguration
+    document.dispatchEvent(new CustomEvent(this.constructor.EVENT_INIT, { bubbles: true }));
+
     this.build();
     document.body.appendChild(this.element);
 
